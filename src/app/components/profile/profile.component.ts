7c9e7d168708768e1323a71ff132dbd3690f3274
JesrielLayug <layugjesriel61@gmail.com>
2024-03-14 15:20:22 +0800

    Finalizing profile section and adding edit profile.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { EditProfileComponent } from '../edit-profile/edit-profile.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [EditProfileComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
